52ce13c0ed8f467de78ef60a5153a9491c30bd23
isaacau <yau@andrew.cmu.edu>
2025-10-03 15:08:53 -0400

    Revert "autofullscreen"
    This reverts commit ae1bed69044d44af8e91cb307b9dd3a6cd29eb81.

--- ui-revamp/src/App.jsx
+++ ui-revamp/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import { useBodyClasses } from "./hooks/useBodyClasses";
-import { useFullscreen } from "./hooks/useFullscreen";
 import { renderRootRedirect } from "./utils/routeHelpers.jsx";
 
 // Route Components
@@ -14,9 +13,6 @@ function App() {
 
   // Apply body classes based on current route
   useBodyClasses();
-
-  // Handle fullscreen on mobile devices
-  useFullscreen();
 
   if (isAuthLoading) return null;
 
